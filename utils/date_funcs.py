efd6faef8c155362c7d19edf578465dd5e3da214
Mahmoud Ashraf <mahmoud.asraf2000@yahoo.com>
2023-06-08 16:50:36 +0300

    Edit Warning date format

--- utils/date_funcs.py
+++ utils/date_funcs.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 
+
 def current_datetime():
-    formatted_date = datetime.now().strftime("%A-%m-%Y %H:%M:%S")
+    formatted_date = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
     return formatted_date
+
 
